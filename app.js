453f1d582e543d6f4ed2afaa86435d7e6023bca7
Wikkie <vigneshrgm962950@gmail.com>
2023-01-11 22:14:26 +0530

    Added Login page

--- app.js
+++ app.js
@@ -1,25 +1,22 @@
 import express from 'express';
-
 import ejs from 'ejs';
 import path from 'path';
-
-
 const PORT = 3000
 const app = express();
 const __dirname = path.resolve();
 
-app.set('view engine', 'ejs',path)
-app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs',path);
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')))
+
 
 app.get('/',(req,res)=>{
     res.render('index');
 })
 
-
-
-
-
+app.get('/login',(req,res)=>{
+    res.render('login');
+})
 
 app.listen(PORT, err =>{
     if(err) console.log(err)
